42e32bafd4a86c7dd349cf30bd26adea9d92ba12
luowp <luowp@yczcjk.com>
2021-06-28 16:25:31 +0800

    -feat: 修改版本号 version v0.2-beta.6

--- src/version.js
+++ src/version.js
@@ -2,7 +2,7 @@
  * 当前版本信息
  */
 const Version = 'v0.2';
-const BetaNumber = '.5';
+const BetaNumber = '.6';
 const info = `Version：${Version}${BetaNumber ? `-beta${BetaNumber}` : ''}`;
 window.CurrentVersions = info;
 
