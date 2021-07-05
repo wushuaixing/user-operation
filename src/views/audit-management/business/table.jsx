f33ac0df70aa6a8ec74efdd90d949585fc8cdc6d
xiyu <zhangy@yczcjk.com>
2021-07-05 13:38:02 +0800

    feat(audit-management) [客户使用机构列-增加显示字段]

--- src/views/audit-management/business/table.jsx
+++ src/views/audit-management/business/table.jsx
@@ -6,11 +6,20 @@ import AuctionInfo from '@/components/column-pane/auction-info';
 const columnHtml = (props, tabType) => {
   const { status = '' } = props || {};
   const obj = PUSH_STATUS.find((i) => status === i.value) || {};
+  const levelType = {
+    3: '一级',
+    4: '二级',
+    5: '三级',
+    6: '四级',
+    7: '五级',
+    8: '六级',
+  };
+  const levelHtml = levelType[props.level] ? `【${levelType[props.level]}】` : '';
   return {
     assetInfo: AssetInfo(props),
     remarkInfo: RemarkInfo(props, tabType),
     auctionInfo: AuctionInfo(props),
-    conSumerName: props.conSumerName,
+    conSumerName: `${levelHtml}${props.conSumerName || ''}`,
     status: obj.label,
   };
 };
