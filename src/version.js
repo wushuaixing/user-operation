fb8e27036eb9750995aa777b189e37edc4e65b0e
xiyu <zhangy@yczcjk.com>
2021-07-08 11:01:32 +0800

    feat(version)：修改版本号v0.2 beta.13

--- src/version.js
+++ src/version.js
@@ -2,7 +2,7 @@
  * 当前版本信息
  */
 const Version = 'v0.2';
-const BetaNumber = '.12';
+const BetaNumber = '.13';
 const info = `Version：${Version}${BetaNumber ? `-beta${BetaNumber}` : ''}`;
 window.CurrentVersions = info;
 
