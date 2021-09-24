4f05a14149e1a7d545351fa7ba2a6c18fa8eb9b2
王川 <2460856147@qq.com>
2021-09-24 10:16:42 +0800

    feat[monitor-view]全局样式改为局部样式

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,6 @@ export default {
   color: #20242e;
   height: 100vh;
 }
-
 .yc-newpage-contaner {
   background-color: #edeff3;
   min-height: 100%;
@@ -33,26 +32,6 @@ export default {
         color: #20242E;
       }
     }
-  }
-}
-.el-radio-button__inner{
-  padding: 9px 15px!important;
-  font-weight: normal!important;
-  border-color: #DDE0E7!important;
-  box-shadow: unset!important;
-}
-.el-radio-button:first-child .el-radio-button__inner{
-  border-radius: 2px 0px 0px 2px!important;
-}
-.el-radio-button:last-child .el-radio-button__inner{
-  border-radius: 0 2px 2px 0!important;
-}
-.el-radio-button__orig-radio:checked + .el-radio-button__inner{
-  color: #296DD3!important;
-  background-color: #E7F1FF!important;
-  border-color: #E7F1FF!important;
-  &~.el-radio-button__inner{
-    border-color: #DDE0E7!important;
   }
 }
 </style>
